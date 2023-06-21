4cf7d1ad1e5806c3908ba4a40cf20d15f2cdda2b
alfrdfrd <roialvarico@gmail.com>
2023-06-21 21:49:08 +0800

    Added guess counter

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,13 @@ fn main() {
     println!("Guess the Number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    
+    let mut counter: u32 = 0;
     loop{
+        println!("Guess Count: {counter}");
         println!("Please input your guess.");
     
         let mut guess = String::new();
-    
+
         io::stdin().read_line(&mut guess).expect("Failed to read line");
 
         let guess: u32 = match guess.trim().parse() {
@@ -22,8 +23,14 @@ fn main() {
         println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number){
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too large!"),
+        Ordering::Less => {
+            counter += 1;
+            println!("Too small!")
+        },
+        Ordering::Greater => {
+            counter += 1;
+            println!("Too large!")
+        },
         Ordering::Equal => {
             println!("You win!");
             break;            
